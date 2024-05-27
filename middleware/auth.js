5dec04c2aae6aabd0eb807aaa8dc829eac6b4e2c
Jennifer Simond <jenniasimond@gmail.com>
2024-05-26 20:18:13 -0400

    create isCustomer, isSiteAdmin, isSuperAdmin functions

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,63 @@
 var jwt = require('jsonwebtoken');
+const { findUserWithToken } = require('../database/database.js');
 const secret = process.env.JWT_SECRET || 'shhhhhlocal';
 
-const authentication = async (req, res, nxr) => {
+const userAuthentication = async (req, res, next) => {
   try {
-  } catch (error) {}
+    if (!req.headers.authorization) {
+      throw new Error('Not Authorized!');
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    const user = await findUserWithToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Not Authorized' });
+  }
+};
+
+// Create site admin middleware
+
+const isSiteAdmin = (req, res, next) => {
+  try {
+    if (req.user.role !== 'site_admin' && req.user.role !== 'super_admin') {
+      throw new Error('Not Authorized');
+    }
+    next();
+  } catch (error) {
+    res.status(403).json({ message: 'Forbbiden' });
+  }
+};
+// Create super admin middle ware
+const isSuperAdmin = (req, res, next) => {
+  try {
+    if (req.user.role !== 'super_admin') {
+      throw new Error('Not Authorized');
+    }
+    next();
+  } catch (error) {
+    res.status(403).json({ message: 'Forbbiden' });
+  }
+};
+// Creaete authorized customer middle ware
+const isAuthorizedCustomer = (req, res, next) => {
+  try {
+    if (
+      req.user.role !== 'customer' &&
+      req.user.role !== 'site_admin' &&
+      req.user.role !== 'super_admin'
+    ) {
+      throw new Error('Not Authorized');
+    }
+    next();
+  } catch (error) {
+    res.status(403).json({ message: 'Forbidden' });
+  }
+};
+
+module.exports = {
+  userAuthentication,
+  isSiteAdmin,
+  isSuperAdmin,
+  isAuthorizedCustomer,
 };
