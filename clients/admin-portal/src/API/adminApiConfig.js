7f80268b630bd44e2c6eef21e638f880035cb78a
Jennifer Simond <jenniasimond@gmail.com>
2024-07-02 14:41:55 -0400

    update API configs

--- clients/admin-portal/src/API/adminApiConfig.js
+++ clients/admin-portal/src/API/adminApiConfig.js
@@ -1,5 +1,5 @@
 // export const API_URL = 'http://localhost:3000/api';
 // export const BASE_URL = 'http://localhost:3000';
 
-export const API_URL = 'https://chic-haven-backend.onrender.com/api';
-export const BASE_URL = 'https://chic-haven-backend.onrender.com';
+export const API_URL = 'https://chic-haven-boutique-backend.onrender.com/api';
+export const BASE_URL = 'https://chic-haven-boutique-backend.onrender.com';
