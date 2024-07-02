1a0b2f08655aa32ea849c1a3cb695ca9e7c37b94
Jennifer Simond <jenniasimond@gmail.com>
2024-07-02 03:57:18 -0400

    update front end API urls to deployed backend

--- clients/admin-portal/src/API/apiConfig.js
+++ clients/admin-portal/src/API/apiConfig.js
@@ -1,2 +1,5 @@
-export const API_URL = 'http://localhost:3000/api';
-export const BASE_URL = 'http://localhost:3000';
+// export const API_URL = 'http://localhost:3000/api';
+// export const BASE_URL = 'http://localhost:3000';
+
+export const API_URL = 'https://chic-haven-backend.onrender.com/api';
+export const BASE_URL = 'https://chic-haven-backend.onrender.com';
